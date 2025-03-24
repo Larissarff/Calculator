39026d13328c0e9067190f0020eda509d74f9131
LARISSA RODRIGUES FONTOURA FERREIRA <06011175@acad.unifeso.lan>
2025-03-24 18:17:56 -0300

    fix: adjusting the limit of the calculator buttons and screen

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ cor_preta = "#0a080a"
 
 janela = Tk()
 janela.title("Calculadora")
-janela.geometry("265x358") # primeiro valor para largura da tela e o segundo para o comprimento
+janela.geometry("265x310") # primeiro valor para largura da tela e o segundo para o comprimento
 janela.config(bg= cor_rosa)
 
 
@@ -20,12 +20,12 @@ janela.config(bg= cor_rosa)
 frame_display = Frame(janela, width=265, height=50, bg= cor_rosa_escuro)
 frame_display.grid(row=0, column=0) 
 
-frame_corpo = Frame(janela, width=265, height=308, bg=cor_rosa_escuro)
+frame_corpo = Frame(janela, width=265, height=260, bg=cor_rosa_escuro)
 frame_corpo.grid(row=1, column=0)
 
 
 # variável de todos os valores
-todos_valores = '12+99999'
+todos_valores = ''
 
 
 # criando funções:
@@ -69,66 +69,66 @@ app_label.place(x=0,y=0)
 
 
 # PRIMEIRA LINHA
-btn_clear = Button(frame_corpo, command= limpar_tela, text= "C", width=9, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_clear = Button(frame_corpo, command= limpar_tela, text= "C", width=10, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 btn_clear.place(x=0, y=0)
 
-btn_percent = Button(frame_corpo, command= lambda: entrar_valores('%'), text= "%", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_percent.place(x=115, y=0)
+btn_percent = Button(frame_corpo, command= lambda: entrar_valores('%'), text= "%", width=6, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_percent.place(x=110, y=0)
 
-btn_barra = Button(frame_corpo, command= lambda: entrar_valores('/'), text= "/", width=5, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_barra.place(x=190, y=0)
+btn_barra = Button(frame_corpo, command= lambda: entrar_valores('/'), text= "/", width=8, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_barra.place(x=180, y=0)
 
 
 # SEGUNDA LINHA
-btn_seven = Button(frame_corpo, command= lambda: entrar_valores('7'), text= "7", width=4, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_seven.place(x=0, y=62)
+btn_seven = Button(frame_corpo, command= lambda: entrar_valores('7'), text= "7", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_seven.place(x=0, y=52)
 
-btn_eight = Button(frame_corpo, command= lambda: entrar_valores('8'), text= "8", width=4, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_eight.place(x=64, y=62)
+btn_eight = Button(frame_corpo, command= lambda: entrar_valores('8'), text= "8", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_eight.place(x=61, y=52)
 
-btn_nine = Button(frame_corpo, command= lambda: entrar_valores('9'), text= "9", width=4, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_nine.place(x=129, y=62)
+btn_nine = Button(frame_corpo, command= lambda: entrar_valores('9'), text= "9", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_nine.place(x=122, y=52)
 
-btn_asterisk = Button(frame_corpo, command= lambda: entrar_valores('*'), text= "*", width=5, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_asterisk.place(x=190, y=62)
+btn_asterisk = Button(frame_corpo, command= lambda: entrar_valores('*'), text= "*", width=8, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_asterisk.place(x=183, y=52)
 
 
 # TERCEIRA LINHA
-btn_four = Button(frame_corpo, command= lambda: entrar_valores('4'), text= "4", width=4, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_four.place(x=0, y=124)
+btn_four = Button(frame_corpo, command= lambda: entrar_valores('4'), text= "4", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_four.place(x=0, y=104)
 
-btn_five = Button(frame_corpo, command= lambda: entrar_valores('5'), text= "5", width=4, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_five.place(x=64, y=124)
+btn_five = Button(frame_corpo, command= lambda: entrar_valores('5'), text= "5", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_five.place(x=61, y=104)
 
-btn_six = Button(frame_corpo, command= lambda: entrar_valores('6'), text= "6", width=4, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_six.place(x=129, y=124)
+btn_six = Button(frame_corpo, command= lambda: entrar_valores('6'), text= "6", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_six.place(x=122, y=104)
 
-btn_subtraction = Button(frame_corpo, command= lambda: entrar_valores('-'), text= "-", width=5, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_subtraction.place(x=190, y=124)
+btn_subtraction = Button(frame_corpo, command= lambda: entrar_valores('-'), text= "-", width=8, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_subtraction.place(x=183, y=104)
 
 
 # QUARTA LINHA
-btn_one = Button(frame_corpo, command= lambda: entrar_valores('1'), text= "1", width=4, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_one.place(x=0, y=186)
+btn_one = Button(frame_corpo, command= lambda: entrar_valores('1'), text= "1", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_one.place(x=0, y=156)
 
-btn_two = Button(frame_corpo, command= lambda: entrar_valores('2'), text= "2", width=4, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_two.place(x=64, y=186)
+btn_two = Button(frame_corpo, command= lambda: entrar_valores('2'), text= "2", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_two.place(x=61, y=156)
 
-btn_three = Button(frame_corpo, command= lambda: entrar_valores('3'), text= "3", width=4, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_three.place(x=129, y=186)
+btn_three = Button(frame_corpo, command= lambda: entrar_valores('3'), text= "3", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_three.place(x=122, y=156)
 
-btn_addition = Button(frame_corpo, command= lambda: entrar_valores('+'), text= "+", width=5, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_addition.place(x=190, y=186)
+btn_addition = Button(frame_corpo, command= lambda: entrar_valores('+'), text= "+", width=8, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_addition.place(x=183, y=156)
 
 
 # QUINTA LINHA
-btn_zero = Button(frame_corpo, command= lambda: entrar_valores('0'), text= "0", width=9, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_zero.place(x=0, y=248)
+btn_zero = Button(frame_corpo, command= lambda: entrar_valores('0'), text= "0", width=10, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_zero.place(x=0, y=208)
 
-btn_point = Button(frame_corpo, command= lambda: entrar_valores('.'), text= ".", width=5, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_point.place(x=115, y=248)
+btn_point = Button(frame_corpo, command= lambda: entrar_valores('.'), text= ".", width=6, height=2, bg = cor_rosa_muito_claro, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_point.place(x=110, y=208)
 
-btn_equal = Button(frame_corpo, command= calcular, text= "=", width=5, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-btn_equal.place(x=190, y=248)
+btn_equal = Button(frame_corpo, command= calcular, text= "=", width=8, height=2, bg = cor_rosa, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn_equal.place(x=180, y=208)
 
 janela.mainloop()
